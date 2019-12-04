a90af37ae2b77d5e785c7f86211ba64bfa4d3175
Daniel-Pivonka <dpivonka@redhat.com>
2019-12-04 16:45:58 -0500

    add simple comments

--- timbot.py
+++ timbot.py
@@ -8,19 +8,24 @@ channel_name = 'GMS5DNGSK'
 sc = SlackClient(slack_token)
 
 while True:
-
 	history = sc.api_call("groups.history", channel=channel_name, count=1)
 
+	# look for a message in the chat that starts with '@timbot .....'
 	if 'messages' in history:
 		for message in  history['messages']:
 			if 'text' in message:
 				message = message['text']
 				message = message.encode('UTF8')
 				if message.startswith('<@UR9HA8KV2>'):
+
+					#message now equals what was after '@timbot'
 					message = message.replace('<@UR9HA8KV2>', '')
 
+					#check what the message is
 					if 'lunch' in message:
+						#send a message as timbot
 						sc.api_call("chat.postMessage", channel=channel_name, text="11:30", as_user=True)
 					else:
+						#send a message as timbot
 						sc.api_call("chat.postMessage", channel=channel_name, text="keep pounding", as_user=True)
 
