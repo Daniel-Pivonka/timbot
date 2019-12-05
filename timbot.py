2478df8adb53aa620228be378af83fd07b6b2262
Nathan Weinberg <nathan2@stwmd.net>
2019-12-05 16:14:43 -0500

    Fixed styling in timbot.py
    Signed-off-by: Nathan Weinberg <nathan2@stwmd.net>

--- timbot.py
+++ timbot.py
@@ -13,14 +13,9 @@ alert = True
 ideal_lunch_time = "11:30"
 friday_index_elem = 4
 
-if __name__ == '__main__':
-	main()
-
-
 def main():
 	while True:
 		run_timbot()
-
 
 def handle_ideal_lunch_time(curr_time):
 	if curr_time == ideal_lunch_time and alert:
@@ -41,43 +36,45 @@ def run_timbot():
 				message = message['text']
 				message = message.encode('UTF8')
 
-				#openstack meme
+				# openstack meme
 				if 'openstack' in message:
 					sc.api_call("chat.postMessage", channel=channel_name, text='i hear opensack is a career killer', as_user=True)
 
-				#pong responce
+				# pong responce
 				elif 'pong' in message:
 					sc.api_call("chat.postMessage", channel=channel_name, text='im in. best 2 out of 3 games to 7?', as_user=True)
 
-
 				if message.startswith(timbot_user_id):
 
-					#message now equals what was after '@timbot'
+					# message now equals what was after '@timbot'
 					message = message.replace(timbot_user_id, '')
 
-					#where to go to lunch
+					# where to go to lunch
 					if 'lunch' in message and 'where' in message:
 
-						#if friday
+						# if friday
 						if datetime.datetime.today().weekday() == friday_index_elem:
 							random.seed(datetime.datetime.now())
 
-							#choose place to go
+							# choose place to go
 							places = ['pauls','moes','asian plus','99s']
 							sc.api_call("chat.postMessage", channel=channel_name, text=random.choice(places), as_user=True)
 
-						#mon-thur
+						# mon-thur
 						else:
 							sc.api_call("chat.postMessage", channel=channel_name, text='epicurean feast', as_user=True)
 
-					#what time is lunch
+					# what time is lunch
 					elif 'lunch' in message and ('time' in message or 'when' in message):
 						sc.api_call("chat.postMessage", channel=channel_name, text=ideal_lunch_time, as_user=True)
 
-					#what to eat
+					# what to eat
 					elif 'what' in message and 'eat' in message:
 						sc.api_call("chat.postMessage", channel=channel_name, text="chicken sandwich", as_user=True)
 
-					#base response
+					# base response
 					else:
 						sc.api_call("chat.postMessage", channel=channel_name, text="keep pounding", as_user=True)
+
+if __name__ == '__main__':
+	main()
