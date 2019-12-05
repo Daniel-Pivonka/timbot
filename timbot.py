c9cc90ae6b13c28604e84c092cdc531106810f30
Daniel-Pivonka <dpivonka@redhat.com>
2019-12-05 13:31:45 -0500

    lunch alert and comments

--- timbot.py
+++ timbot.py
@@ -9,9 +9,22 @@ channel_name = 'GMS5DNGSK'
 
 sc = SlackClient(slack_token)
 
+random.seed(datetime.datetime.now())
+
+alert = True
+
 while True:
 	history = sc.api_call("groups.history", channel=channel_name, count=1)
 
+	#lunch alert
+	time = datetime.datetime.now().strftime('%H:%M')
+	if time == '11:30':
+		if alert:
+			sc.api_call("chat.postMessage", channel=channel_name, text='IT IS THE IDEAL LUNCH TIME GO TO LUNCH', as_user=True)
+			alert = False
+	else:
+		alert = True
+					
 	# look for a message in the chat that starts with '@timbot .....'
 	if 'messages' in history:
 		for message in  history['messages']:
@@ -19,6 +32,7 @@ while True:
 				message = message['text']
 				message = message.encode('UTF8')
 
+				#openstack meme
 				if 'openstack' in message:
 					sc.api_call("chat.postMessage", channel=channel_name, text='i hear opensack is a career killer', as_user=True)
 
@@ -28,18 +42,24 @@ while True:
 					#message now equals what was after '@timbot'
 					message = message.replace('<@UR9HA8KV2>', '')
 
-					#check what the message is
+					#where to go to lunch
 					if 'lunch' in message and 'where' in message:
-						#send a message as timbot
+						
+						#if friday
 						if datetime.datetime.today().weekday() == 4:
+							#choose place to go
 							places = ['pauls','moes','asian plus','99s']
 							sc.api_call("chat.postMessage", channel=channel_name, text=random.choice(places), as_user=True)
+
+						#mon-thur 
 						else:
 							sc.api_call("chat.postMessage", channel=channel_name, text='epicurean feast', as_user=True)
+
+					#what time is lunch
 					elif 'lunch' in message and ('time' in message or 'when' in message):
-						#send a message as timbot
 						sc.api_call("chat.postMessage", channel=channel_name, text="11:30", as_user=True)
+
+					#base response 
 					else:
-						#send a message as timbot
 						sc.api_call("chat.postMessage", channel=channel_name, text="keep pounding", as_user=True)
 
