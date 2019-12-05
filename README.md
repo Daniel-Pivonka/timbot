0dd32b2ab24a9175b1caaaee6dcf851df210c821
timflannagan1 <timflannagan@gmail.com>
2019-12-05 17:27:32 -0500

    Container the timbot application.

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-# timbot
+### Purpose
 A Slack "bot" to replace our dear friend Tim
 
-## References
+### References
 - https://api.slack.com/custom-integrations/legacy-tokens
 - https://slack.dev/python-slackclient/
-- https://api.slack.com/methods 
+- https://api.slack.com/methods
+
+##### Building the container image
+```bash
+$ make build-timbot
+```
+
+##### Running the timbot image locally
+```bash
+$ make run-timbot
+```
---
+++ scripts/run-timbot-local.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+set -e
+
+if [[ -z "$SLACK_API_TOKEN" ]]; then
+    echo "You need to set the \$SLACK_API_TOKEN"
+    exit 1
+elif [[ -z "$SLACK_CHANNEL_NAME" ]]; then
+    echo "You need to set the \$SLACK_CHANNEL_NAME"
+    exit 1
+elif [[ -z "$SLACK_TIMBOT_USER_ID" ]]; then
+    echo "You need to set the \$SLACK_TIMBOT_USER_ID"
+    exit 1
+fi
+
+CONTAINER_RUN_CMD=${CONTAINER_RUN_CMD:-"podman run"}
+
+set -x
+
+${CONTAINER_RUN_CMD} \
+    --name timbot-local \
+    --rm \
+    -e SLACK_API_TOKEN="$SLACK_API_TOKEN" \
+    -e SLACK_CHANNEL_NAME="$SLACK_CHANNEL_NAME" \
+    -e SLACK_TIMBOT_USER_ID="$SLACK_TIMBOT_USER_ID" \
+    -it timbot
